5f0393f00ddeb7939a2559f90df4fc7d95a860f1
Rom0105 <savosko.photo@gmail.com>
2021-10-16 19:07:42 +0300

    правки

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,81 +1,142 @@
 import React, { Component } from 'react';
-import Container from '../Container/Container';
-import ContactForm from '../ContactForm/ContactForm';
-import Filter from '../Filter/Filter';
-import ContactList from '../ContactList/ContactList';
-import style from '../App/App.module.css';
+import PropTypes from 'prop-types';
+import { error } from '@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+import 'react-toastify/dist/ReactToastify.css';
+import Searchbar from '../Searchbar/Searchbar';
+import ImageGallery from '../ImageGallery/ImageGallery';
+import Modal from '../Modal/Modal';
+import pixHandler from '../../Services/AppiServise';
+import Loader from '../Loader/Loader';
+import Button from '../Button/Button';
 
 class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+  static propTypes = {
+    searchQuery: PropTypes.string,
   };
 
-  addContact = newContact => {
-    const normalizedName = newContact.name.toLowerCase();
-    const duplicateName = this.state.contacts.find(
-      contact => contact.name.toLowerCase() === normalizedName,
-    );
+  state = {
+    searchQuery: '',
+    page: 1,
+    images: [],
+    selectedImg: null,
+    alt: null,
+    status: 'idle',
+  };
 
-    if (duplicateName) {
-      alert(`${newContact.name} is already on contacts`);
+  async componentDidUpdate(_, prevState) {
+    const prevSearch = prevState.searchQuery;
+    const nextSearch = this.state.searchQuery;
+
+    const prevPage = prevState.page;
+    const nextPage = this.state.page;
+
+    if (prevSearch !== nextSearch || prevPage !== nextPage) {
+      this.setState({ status: 'pending' });
+
+      try {
+        const images = await pixHandler(nextSearch, nextPage);
+
+        if (!images.length) {
+          error({
+            text: 'No image!',
+            delay: 1000,
+          });
+        }
+
+        this.setState(prevState => ({
+          images: [...prevState.images, ...images],
+          status: 'resolved',
+        }));
+      } catch (error) {
+        error({
+          text: 'No image!',
+          delay: 1000,
+        });
+
+        this.setState({ status: 'rejected' });
+      }
+
+      this.state.page > 1 && Button();
+    }
+  }
+
+  handleFormSubmit = searchQuery => {
+    if (this.state.searchQuery === searchQuery) {
       return;
     }
 
-    this.setState(({ contacts }) => ({
-      contacts: [newContact, ...contacts],
-    }));
+    this.resetState();
+    this.setState({ searchQuery });
   };
 
-  changeFilter = event => {
-    this.setState({ filter: event.currentTarget.value });
-  };
-
-  filterContacts = () => {
-    const { contacts, filter } = this.state;
-
-    const normalizedFilter = filter.toLowerCase();
-
-    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
-  };
-
-  deleteContact = id => {
+  loadMoreBtnClick = () => {
     this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
+      page: prevState.page + 1,
     }));
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  handleSelectedImage = (largeImageUrl, tags) => {
+    this.setState({
+      selectedImg: largeImageUrl,
+      alt: tags,
+    });
+  };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  closeModal = () => {
+    this.setState({
+      selectedImg: null,
+    });
+  };
+
+  resetState = () => {
+    this.setState({
+      searchQuery: '',
+      page: 1,
+      images: [],
+      selectedImg: null,
+      alt: null,
+      status: 'idle',
+    });
+  };
 
   render() {
-    const { filter } = this.state;
-    const filteredResults = this.filterContacts();
-    return (
-      <Container>
-        <h2 className={style.title}>Phonebook</h2>
-        <ContactForm onSubmit={this.addContact} />
-        <h2 className={style.title}>Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter} />
-        <ContactList contacts={filteredResults} onDeleteContact={this.deleteContact} />
-      </Container>
-    );
+    const { images, selectedImg, alt, status } = this.state;
+
+    if (status === 'idle') {
+      return <Searchbar onSubmit={this.handleFormSubmit} />;
+    }
+
+    if (status === 'pending') {
+      return (
+        <>
+          <Searchbar onSubmit={this.handleFormSubmit} />
+          {/* <Spinner /> */}
+          <ImageGallery images={images} selectedImage={this.handleSelectedImage} />
+
+          {images.length > 0 && <Loader onClick={this.loadMoreBtnClick} />}
+        </>
+      );
+    }
+
+    if (status === 'resolved') {
+      return (
+        <>
+          <Searchbar onSubmit={this.handleFormSubmit} />
+          <ImageGallery images={images} selectedImage={this.handleSelectedImage} />
+          {selectedImg && <Modal selectedImg={selectedImg} tags={alt} onClose={this.closeModal} />}
+          {images.length > 0 && <Loader onClick={this.loadMoreBtnClick} />}
+        </>
+      );
+    }
+
+    if (status === 'rejected') {
+      return (
+        <>
+          <Searchbar onSubmit={this.handleFormSubmit} />
+        </>
+      );
+    }
   }
 }
 
